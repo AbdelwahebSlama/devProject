c4d09249dc6a555eed6ace1faedb353af7f1a468
Abdelwaheb Slama <abdelwaheb.slama@yahoo.fr>
2021-10-26 22:47:36 +0100

    Last Commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello !!");
         System.out.println("Hello 22 2 2!!");
+        System.out.println("Hello 22 2 2!!");
+        System.out.println("Hello 22 2 2!!");
+        System.out.println("Hello 22 2 2!!");
 
     }
 }
